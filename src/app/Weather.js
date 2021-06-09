03d7de3d64ec3944731a5d166f1894b6cb74c3a3
Africano Programer <elsbanedu@hotmail.com>
2021-06-09 01:18:12 +0100

    subiendo de nuevo la app de clima

---
+++ src/app/Weather.js
@@ -0,0 +1,19 @@
+export class Weather {
+  constructor(city, countryCode) {
+    this.apiKey = "642b4c69b45deccde7d4ea3ddcd1ad30";
+    this.city = city;
+    this.countryCode = countryCode;
+  }
+
+  async getWeather() {
+    const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}&units=metric`;
+    const response = await fetch(URI);
+    const data = await response.json();
+    return data;
+  }
+
+  changeLocation(city, countryCode) {
+    this.city = city;
+    this.countryCode = countryCode;
+  }
+}
